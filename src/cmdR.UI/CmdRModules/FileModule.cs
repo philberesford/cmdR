2c297b635b68d14d99a6a44e88d62b2a8cd13f62
Andy Long [work] <andy@theouteredge.co.uk>
2013-05-15 16:39:02 +0100

    touch me
    added a touch command, which updates the modified, created or accessed
    datetime stamp of a file

--- src/cmdR.UI/CmdRModules/FileModule.cs
+++ src/cmdR.UI/CmdRModules/FileModule.cs
@@ -13,9 +13,48 @@ namespace cmdR.UI.CmdRModules
     {
         public void Initalise(CmdR cmdR, bool overwriteRoutes)
         {
-            cmdR.RegisterRoute("preview file rows?", Preview, "Previews the first 10 lines of a file, or you can specify the number of rows to read", overwriteRoutes);
+            cmdR.RegisterRoute("head file rows?", Head, "Previews the first n lines of a file, n defaults to 10, but you can specify the number of rows to read", overwriteRoutes);
             cmdR.RegisterRoute("rn match replace", Rename, "Renames a file using a regex match and replace, if you want to run a test first use the switch /test", overwriteRoutes);
             cmdR.RegisterRoute("handles path?", Handles, "Lists the Open Handles on a directory", overwriteRoutes);
+            cmdR.RegisterRoute("touch regex? date?", Touch, "Updates the Last Modified Date of all files in the current directory, you can use /modifed /created /accessed to choose which attributes to modify", overwriteRoutes);
+        }
+
+        private void Touch(IDictionary<string, string> param, CmdR cmdR)
+        {
+            var path = cmdR.State.Variables["path"].ToString();
+            if (Directory.Exists(path))
+            {
+                var modified = param.ContainsKey("/modified");
+                var created = param.ContainsKey("/created");
+                var accessed = param.ContainsKey("/accessed");
+
+                var date = DateTime.Now;
+
+                if (param.ContainsKey("date"))
+                    date = DateTime.Parse(param["date"]);
+
+                if (!modified && !created && !accessed)
+                    modified = true;
+
+                Regex match = null;
+                if (param.ContainsKey("regex"))
+                    match = new Regex(param["regex"]);
+
+                foreach (var file in Directory.GetFiles(path).Where(x => match != null && match.IsMatch(x)))
+                {
+                    if (modified)
+                        File.SetLastWriteTime(file, date);
+
+                    if (created)
+                        File.SetLastAccessTime(file, date);
+
+                    if (accessed)
+                        File.SetCreationTime(file, date);
+
+                    cmdR.Console.WriteLine("touched {0}", file);
+                }
+            }
+            else cmdR.Console.WriteLine("{0} does not exist", path);
         }
 
         private void Handles(IDictionary<string, string> param, CmdR cmdR)
@@ -77,7 +116,7 @@ namespace cmdR.UI.CmdRModules
                 cmdR.Console.WriteLine("{0} files renames with {1} errors", count, errors);
         }
 
-        private void Preview(IDictionary<string, string> param, CmdR cmdR)
+        private void Head(IDictionary<string, string> param, CmdR cmdR)
         {
             var path = Path.Combine((string)cmdR.State.Variables["path"], param["file"]);
             var take = param.ContainsKey("rows") ? int.Parse(param["rows"]) : 10;
