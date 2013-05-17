38d18f3b7a3a2229fb030beb75aed49026777c62
Andy Long <andy@theouteredge.co.uk>
2013-05-17 23:42:06 +0100

    RichTextbox
    Completed the implementation of the RichTextbox as the output section of
    the CmdR.UI you can now have colours and everything!

--- src/cmdR.UI/ViewModels/WpfConsole.cs
+++ src/cmdR.UI/ViewModels/WpfConsole.cs
@@ -10,6 +10,7 @@ namespace cmdR.UI.ViewModels
         public WpfConsole(IWpfViewModel viewModel)
         {
             _viewmodel = viewModel;
+            _viewmodel.Output = "<Section xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xml:space=\"preserve\" LineHeight=\"1\">\n</Section>";
         }
 
         public string ReadLine()
@@ -17,24 +18,40 @@ namespace cmdR.UI.ViewModels
             throw new NotImplementedException();
         }
 
+        private string WrapText(string text, params object[] param)
+        {
+            text = text.StartsWith("<Run") ? string.Format(text, param) : string.Format("<Run>{0}</Run>", string.Format(text, param).XmlEscape());
+            return text + "</Paragraph>";
+        }
+
         public void Write(string line, params object[] param)
         {
-            _viewmodel.Output = string.Format("{0}{1}", _viewmodel.Output, string.Format(line, param));
+            // we don't have a paragraph to append any text too, so just do a write line
+            if (!_viewmodel.Output.Contains("<Paragraph>"))
+                WriteLine(line, param);
+
+             _viewmodel.Output = _viewmodel.Output.Replace("</Paragraph>\n</Section>", string.Format("\t{0}\n</Section>", WrapText(line, param)));
         }
 
         public void Write(string line)
         {
-            _viewmodel.Output = string.Format("{0}{1}", _viewmodel.Output, line);
+            // we don't have a paragraph to append any text too, so just do a write line
+            if (!_viewmodel.Output.Contains("<Paragraph>"))
+                WriteLine(line);
+
+            _viewmodel.Output = _viewmodel.Output.Replace("</Paragraph>\n</Section>", string.Format("\t{0}\n</Section>", WrapText(line)));
         }
 
         public void WriteLine(string line, params object[] param)
         {
-            _viewmodel.Output = string.Format("{0}\n{1}", _viewmodel.Output, string.Format(line, param));
+            var text = string.Format("<Paragraph><Run>{0}</Run></Paragraph>", string.Format(line, param));
+            _viewmodel.Output = _viewmodel.Output.Replace("</Section>", string.Format("\t<Paragraph>{0}\n</Section>", WrapText(line, param)));
         }
 
         public void WriteLine(string line)
         {
-            _viewmodel.Output = string.Format("{0}\n{1}", _viewmodel.Output, line);
+            var text = string.Format("<Paragraph><Run>{0}</Run></Paragraph>", line);
+            _viewmodel.Output = _viewmodel.Output.Replace("</Section>", string.Format("\t<Paragraph>{0}\n</Section>", WrapText(line)));
         }
     }
 }
